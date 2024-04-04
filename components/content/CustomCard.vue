de50400212f0fb9717b64eb0c982c8c77457f276
muhammederdinc <erdincmuhammed@gmail.com>
2024-04-04 03:03:37 +0300

    chore: Refactor CustomCard component to include title and text props

--- components/content/CustomCard.vue
+++ components/content/CustomCard.vue
@@ -1,10 +1,39 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: "Title",
+  },
+  text: {
+    type: String,
+    default: "Text",
+  },
+});
+</script>
+
 <template>
-  <div class="card">This is a custom card component</div>
+  <div class="card">
+    <div class="card__title">
+      {{ title }}
+    </div>
+
+    <div>
+      {{ text }}
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .card {
-  border: 1px solid black;
+  border: 1px solid white;
   border-radius: 10px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column;
+}
+
+.card__title {
+  font-size: 24px;
+  font-weight: bold;
 }
 </style>
